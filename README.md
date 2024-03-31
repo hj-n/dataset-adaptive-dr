a00f6d5a824124331013c5f4638ad71adddc9b50
Hyeon Jeon <hj@hcil.snu.ac.kr>
2024-03-31 14:36:55 +0900

    added readme

---
+++ README.md
@@ -0,0 +1,10 @@
+## Structural Complexity Metrics 
+
+들어가야 하는 내용 
+- 이것이 뭔지 대충 설명 (논문 참조)
+- 두개의 metric에 대한 대충 설명
+- 두개의 metric API
+- clone해서 dependency 깔고 쓰는법 (대충 import된 라이브러리 보고 쓰셈요)
+- 그외 리드미를 풍부하게 보일수있게 할수잇는건 무엇이든지
+
++ 각 함수 코드에서 인풋 / 아웃풋에 대해 주석 달아주시면 되겠습니
---
+++ src/pds.py
@@ -0,0 +1,17 @@
+from scipy.spatial.distance import cdist, pdist
+import numpy as np
+from knnsnn import DistMat as DistMat
+from numba import cuda
+
+
+def pairwise_distnace_shift(data):
+	"""
+	Pairwise distance shift: complexity metric targetting global structure
+	
+	### TODO ###
+
+	"""
+	dist_matrix = DistMat().distance_matrix(data)
+	dist_matrix = dist_matrix.flatten()
+	dist_matrix = dist_matrix[dist_matrix > 0]
+	return np.std(dist_matrix) / np.mean(dist_matrix)
